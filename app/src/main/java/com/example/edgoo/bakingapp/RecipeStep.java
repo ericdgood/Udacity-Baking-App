2a91254887b2fb7d8b61e770a75d2a4a725fb22c
Eric Good <ericdgood@yahoo.com>
2018-11-10 12:42:10 -0500

    added buttons, needs limit on next button

--- app/src/main/java/com/example/edgoo/bakingapp/RecipeStep.java
+++ app/src/main/java/com/example/edgoo/bakingapp/RecipeStep.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -14,14 +15,20 @@ import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static android.content.ContentValues.TAG;
 
 public class RecipeStep extends AppCompatActivity {
 
-    @BindView(R.id.step_description) TextView description;
-    @BindView(R.id.previous_step) ImageButton previousStep;
-    @BindView(R.id.next_step) ImageButton nextStep;
+    @BindView(R.id.step_description)
+    TextView description;
+    @BindView(R.id.previous_step)
+    ImageButton previousStep;
+    @BindView(R.id.next_step)
+    ImageButton nextStep;
+    int currentStepDisplay;
+    int arrayListSize;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,11 +36,28 @@ public class RecipeStep extends AppCompatActivity {
         setContentView(R.layout.recipe_step);
         ButterKnife.bind(this);
 
-        ArrayList descriptionsArray = getIntent().getStringArrayListExtra("description");
-        String step_id = getIntent().getStringExtra("step_id");
+        final ArrayList descriptionsArray = getIntent().getStringArrayListExtra("description");
+        arrayListSize = descriptionsArray.size();
+        Log.i(TAG, "onCreate: " + arrayListSize);
+        int step_id = Integer.parseInt(getIntent().getStringExtra("step_id"));
+        currentStepDisplay = step_id;
 
 //        GETS STEP FROM ARRAY WITH STEPID AND DISPLAYS
-        description.setText((CharSequence) descriptionsArray.get(Integer.parseInt(step_id)));
+        description.setText((CharSequence) descriptionsArray.get(step_id));
 
+        nextStep.setOnClickListener(v -> {
+            if (currentStepDisplay < arrayListSize) {
+            currentStepDisplay = currentStepDisplay + 1;
+                    description.setText((CharSequence) descriptionsArray.get(currentStepDisplay));
+                }
+            });
+
+        previousStep.setOnClickListener(v -> {
+            if (currentStepDisplay >= 1) {
+            currentStepDisplay = currentStepDisplay - 1;
+            description.setText((CharSequence) descriptionsArray.get(currentStepDisplay));
+            }
+        });
     }
 }
+
